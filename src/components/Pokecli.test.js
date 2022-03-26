f48b229fa5557865d13f3f1516991bc45fdc1c7d
Rick <ricksmycodeguy@gmail.com>
2022-03-26 16:50:49 -0500

    wip

---
+++ src/components/Pokecli.test.js
@@ -0,0 +1,35 @@
+// import { render, screen } from '@testing-library/react';
+// import Pokecli from '.Pokecli';
+
+// test('renders learn react link', () => {
+//   render(<Pokecli />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import Pokecli from './Pokecli';
+
+const utils = require(".Pokecli");
+
+const testpoke = {
+  name: "bulbasaur",
+  url: "https://pokeapi.co/api/v2/pokemon/1/",
+  height: 7,
+  weight: 69,
+}
+
+test("renders without error", () => {
+  render(<Pokecli poke={testpoke} />)
+});
+
+// test("height array to be empty at the start of execution", () => {
+//   expect(utils.height.length).toBe(0);
+// });
+
+// test("height array not empty at the end of execution", () => {
+//   // sleep();
+//   expect(utils.height.length.not).toBe(0);
+// });
+
